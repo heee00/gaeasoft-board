c80a841fff84b6f7baa0e47f0d1eec90e2f3fc89
hwheo <jesicaheo@naver.com>
2024-06-07 15:26:53 +0900

    접근제어자를 public으로 변경한다

--- src/main/java/com/gaeasoft/project/dto/BoardDTO.java
+++ src/main/java/com/gaeasoft/project/dto/BoardDTO.java
@@ -2,71 +2,26 @@ package com.gaeasoft.project.dto;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDTO {
 	
-	private Long id;
-	private String writer;
-	private String password;
-	private String title;
-	private String content;
-	private Timestamp writeTime;
-	private int views;
-	private int rowNum;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Timestamp getWriteTime() {
-		return writeTime;
-	}
-	public void setWriteTime(Timestamp writeTime) {
-		this.writeTime = writeTime;
-	}
-	public int getViews() {
-		return views;
-	}
-	public void setViews(int views) {
-		this.views = views;
-	}
-	public int getRowNum() {
-		return rowNum;
-	}
-	public void setRowNum(int rowNum) {
-		this.rowNum = rowNum;
-	}
+	public Long id;
+	public String writer;
+	public String password;
+	public String title;
+	public String content;
+	public Timestamp writeTime;
+	public int views;
+	public int rowNum;
 	
 }
--- src/main/java/com/gaeasoft/project/dto/PageDTO.java
+++ src/main/java/com/gaeasoft/project/dto/PageDTO.java
@@ -1,42 +1,21 @@
 package com.gaeasoft.project.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PageDTO {
 	
-	private int page;			// 현재 페이지
-	private int maxPage;		// 전체 필요한 페이지 갯수
-	private int startPage;	// 현재 페이지 기준 시작 페이지 값
-	private int endPage;		// 현재 페이지 기준 마지막 페이지 값
-	
-	public int getPage() {
-		return page;
-	}
-	public void setPage(int page) {
-		this.page = page;
-	}
-	public int getMaxPage() {
-		return maxPage;
-	}
-	public void setMaxPage(int maxPage) {
-		this.maxPage = maxPage;
-	}
-	public int getStartPage() {
-		return startPage;
-	}
-	public void setStartPage(int startPage) {
-		this.startPage = startPage;
-	}
-	public int getEndPage() {
-		return endPage;
-	}
-	public void setEndPage(int endPage) {
-		this.endPage = endPage;
-	}
+	public int page;			// 현재 페이지
+	public int maxPage;		// 전체 필요한 페이지 갯수
+	public int startPage;	// 현재 페이지 기준 시작 페이지 값
+	public int endPage;		// 현재 페이지 기준 마지막 페이지 값
 	
 }
