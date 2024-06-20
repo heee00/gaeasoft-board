49b17425142c4692409b4b3f9ef6644d609d503a
hwheo <jesicaheo@naver.com>
2024-06-20 09:45:18 +0900

    스케줄러 중복 사용을 제거한다

--- src/main/java/com/gaeasoft/project/batch/QuartzJob.java
+++ src/main/java/com/gaeasoft/project/batch/QuartzJob.java
@@ -3,25 +3,33 @@ package com.gaeasoft.project.batch;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.gaeasoft.project.controller.BoardController;
 
 @Component
 public class QuartzJob implements Job {
 	
 	@Autowired
     private BatchJobLauncher batchJobLauncher;
+    private static final Logger log = LoggerFactory.getLogger(BoardController.class);
 
 	// 주입된 배치 실행기를 사용하여 배치 작업을 실행
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
     	if (batchJobLauncher == null) {
-            System.err.println("BatchJobLauncher is not injected");
+            log.error("BatchJobLauncher 주입 실패");
             return;
         }
     	try {
+            log.info("배치 작업 실행 시작");
             batchJobLauncher.runBatchJob();
+            log.info("배치 작업 실행 완료");
         } catch (Exception e) {
+            log.error("배치 작업 실행 실패", e);
             throw new JobExecutionException("Failed to run batch job", e);
         }
     }
--- src/main/java/com/gaeasoft/project/batch/config/QuartzJobConfig.java
+++ src/main/java/com/gaeasoft/project/batch/config/QuartzJobConfig.java
@@ -1,67 +1,25 @@
 package com.gaeasoft.project.batch.config;
 
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
 import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
-import com.gaeasoft.project.batch.QuartzJob;
-
 @Configuration
-public class QuartzJobConfig {
+@EnableScheduling
+public class QuartzJobConfig extends SpringBeanJobFactory {
 	
 	@Autowired
 	private ApplicationContext applicationContext;
-	 
-	// 스케줄러 팩토리 빈 구성, JobDetail과 Trigger 설정
-	@Bean
-	public SchedulerFactoryBean schedulerFactoryBean() {
-	    SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-	    schedulerFactoryBean.setJobFactory(new AutowiringSpringBeanJobFactory());
-	    schedulerFactoryBean.setJobDetails(quartzJobDetail());
-	    schedulerFactoryBean.setTriggers(jobTrigger());
-	    schedulerFactoryBean.start();
-	    return schedulerFactoryBean;
-	}
-	@Bean
-	public JobDetail quartzJobDetail() {
-	    return JobBuilder.newJob(QuartzJob.class)
-			                     .storeDurably()
-			                     .withIdentity("QuartzJob")
-			                     .withDescription("Invoke Batch Job service...")
-			                     .build();
-	}
-	
-	@Bean
-	public Trigger jobTrigger() {
-	    SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-														                                            .withIntervalInSeconds(60)
-														                                            .repeatForever();
-	    
-	    return TriggerBuilder.newTrigger()
-				                         .forJob(quartzJobDetail())
-				                         .withIdentity("QuartzJobTrigger")
-				                         .withDescription("QuartzJob Trigger")
-				                         .withSchedule(scheduleBuilder)
-				                         .build();
-	}
 	
 	// Quartz에서 사용할 Job 클래스를 Spring Bean으로 자동 주입하는 JobFactory 정의
-	private class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory {
-        @Override
-        protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
-            Object job = super.createJobInstance(bundle);
-            applicationContext.getAutowireCapableBeanFactory().autowireBean(job);
-            return job;
-        }
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+        Object job = super.createJobInstance(bundle);
+        applicationContext.getAutowireCapableBeanFactory().autowireBean(job);
+        return job;
     }
 
 }
