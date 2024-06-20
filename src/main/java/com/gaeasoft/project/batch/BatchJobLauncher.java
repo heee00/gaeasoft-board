4a6cc944d43401be95bc88a533c84309941a0a0c
hwheo <jesicaheo@naver.com>
2024-06-20 13:30:06 +0900

    배치 실행 클래스, 메소드명을 로그 정보에 추가한다

--- src/main/java/com/gaeasoft/project/batch/BatchJobLauncher.java
+++ src/main/java/com/gaeasoft/project/batch/BatchJobLauncher.java
@@ -2,10 +2,13 @@ package com.gaeasoft.project.batch;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.gaeasoft.project.controller.BoardController;
 import com.gaeasoft.project.service.BoardService;
 
 @Component
@@ -13,6 +16,7 @@ public class BatchJobLauncher  {
 	
 	@Autowired
     private BoardService boardService;
+    private static final Logger log = LoggerFactory.getLogger(BoardController.class);
 	
 	@Autowired
     public BatchJobLauncher(BoardService boardService) {
@@ -20,14 +24,17 @@ public class BatchJobLauncher  {
     }
 	
 	// 매일 00:00:00에 실행
-    @Scheduled(cron = "0 0 0 * * *") 
+	@Scheduled(cron = "0 0 0 * * *") 
     public void runBatchJob() throws Exception {
+    	String className = this.getClass().getSimpleName();
+ 		String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
     	LocalDateTime deleteDay = LocalDateTime.now().minusDays(7);
-        System.out.println("Running batch job to delete articles older than: " + deleteDay);
+        log.info("다음 날짜보다 오래된 게시글 삭제 배치 작업 실행 중: {}", deleteDay);
         try {
             boardService.deleteBatchedNoticeArticle(deleteDay);
-            System.out.println("Batch job completed.");
+            log.info(className + "-" + methodName + ": 배치 작업 완료.");
         } catch (Exception e) {
+            log.error(className + "-" + methodName + ": 배치 작업 실행 실패", e);
             System.err.println("Failed to run batch job: " + e.getMessage());
         }
     }
